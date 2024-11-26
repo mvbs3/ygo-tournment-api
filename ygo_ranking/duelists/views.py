edd9ffb13850c11940f5b5a6418b6fa7672bba12
mvbs3 <marcelobs.victor@gmail.com>
2024-11-26 19:48:20 -0300

    criando template base do site e atualizando a html do duelists

--- ygo_ranking/duelists/views.py
+++ ygo_ranking/duelists/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def duelists(request):
-    return HttpResponse('Estou em cliente')
+    return render(request, 'duelists.html')
