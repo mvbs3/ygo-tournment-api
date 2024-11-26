865b5ecea144bfcbe7476ce540054c6e6951a01b
mvbs3 <marcelobs.victor@gmail.com>
2024-11-26 20:54:43 -0300

    adicionando os modelos de duelista, os formularios para preencher e por fim realizando as migrations com makemigrations e migrate

--- ygo_ranking/duelists/models.py
+++ ygo_ranking/duelists/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Duelists(models.Model):
+    name = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    cossyId = models.CharField(max_length=12)
+    victories = models.IntegerField(default=0)
+    defeates = models.IntegerField(default=0)
+    contact_number = models.CharField(max_length=15)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Car(models.Model):
+    name = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField()
+    owner = models.ForeignKey(Duelists, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return super().__str__()
--- ygo_ranking/duelists/views.py
+++ ygo_ranking/duelists/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def duelists(request):
-    return render(request, 'duelists.html')
+def duelists(request: object) -> HttpResponse:
+    
+    if request.method == "GET":
+        return render(request, 'duelists.html')
+    elif request.method == "POST":
+        name = request.POST.get('name')
+        nickname = request.POST.get('nickname')
+        email = request.POST.get('email')
+        cossyId = request.POST.get('cossyId')
+        contact_number = request.POST.get('contact_number')
+        #nao vou usar isso agora mas achei interessante
+        carros =  request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+        anos = request.POST.getlist('ano')
+        
+        print(name, nickname, email, cossyId, contact_number)
+        return HttpResponse("Dados enviados com sucesso")
