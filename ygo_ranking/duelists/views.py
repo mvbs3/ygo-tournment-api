01c89ca0b1d0fee892fe22710d0ca42a76602aca
mvbs3 <marcelobs.victor@gmail.com>
2025-01-16 22:16:58 -0300

    adicionando pagina de criar e atualizar usuarios como staff page

--- ygo_ranking/duelists/views.py
+++ ygo_ranking/duelists/views.py
@@ -8,43 +8,47 @@ from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
 from django.shortcuts import redirect, get_object_or_404
 # Create your views here.
+
 def duelists(request: object) -> HttpResponse:
     
     if request.method == "GET":
         duelists = Duelist.objects.all()
+        
         return render(request, 'duelists.html', {'duelists': duelists})
     elif request.method == "POST":
-        name = request.POST.get('name')
-        nickname = request.POST.get('nickname')
-        email = request.POST.get('email')
-        cossyId = request.POST.get('cossyId')
-        contact_number = request.POST.get('contact_number')
-        #nao vou usar isso agora mas achei interessante
-        deck =  request.POST.getlist('deck')
-        ydkcode = request.POST.getlist('ydkcode')
-        year = request.POST.getlist('year')
-        
-        #checar se o cliente ja existe
-        duelist = Duelist.objects.filter(cossyId=cossyId)
-        if duelist.exists():
-            return HttpResponse("CossyId ja cadastrado")
+        if request.user.is_staff:
+            
+            name = request.POST.get('name')
+            nickname = request.POST.get('nickname')
+            email = request.POST.get('email')
+            cossyId = request.POST.get('cossyId')
+            contact_number = request.POST.get('contact_number')
+            #nao vou usar isso agora mas achei interessante
+            deck =  request.POST.getlist('deck')
+            ydkcode = request.POST.getlist('ydkcode')
+            year = request.POST.getlist('year')
+            
+            #checar se o cliente ja existe
+            duelist = Duelist.objects.filter(cossyId=cossyId)
+            if duelist.exists():
+                return HttpResponse("CossyId ja cadastrado")
 
-        if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
-            return render(request, 'duelists.html', {'name': name, 'nickname': nickname, 'cossyId': cossyId, 'contact_number': contact_number, 'decks': zip(deck, ydkcode,year), 'error': 'Email invalido'})
-        if not re.match(r'^\d{2}9\d{8}$', contact_number):
-            return render(request, 'duelists.html', {'name': name, 'nickname': nickname, 'cossyId': cossyId, 'email': email, 'decks': zip(deck, ydkcode,year), 'error': 'Telefone invalido'})
-        if not re.match(r'^\d{10}$', cossyId):
-            return render(request, 'duelists.html', {'name': name, 'nickname': nickname, 'email': email, 'decks': zip(deck, ydkcode,year),'contact_number': contact_number, 'error': 'CossyId invalido'})
-        
-        duelist = Duelist(name=name, nickname=nickname, email=email, cossyId=cossyId, contact_number=contact_number)
-        duelist.save()
+            if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
+                return render(request, 'duelists.html', {'name': name, 'nickname': nickname, 'cossyId': cossyId, 'contact_number': contact_number, 'decks': zip(deck, ydkcode,year), 'error': 'Email invalido'})
+            if not re.match(r'^\d{2}9\d{8}$', contact_number):
+                return render(request, 'duelists.html', {'name': name, 'nickname': nickname, 'cossyId': cossyId, 'email': email, 'decks': zip(deck, ydkcode,year), 'error': 'Telefone invalido'})
+            if not re.match(r'^\d{10}$', cossyId):
+                return render(request, 'duelists.html', {'name': name, 'nickname': nickname, 'email': email, 'decks': zip(deck, ydkcode,year),'contact_number': contact_number, 'error': 'CossyId invalido'})
+            
+            duelist = Duelist(name=name, nickname=nickname, email=email, cossyId=cossyId, contact_number=contact_number)
+            duelist.save()
 
-        for deck, ydkcode, year in zip(deck, ydkcode, year):
-            deck = Deck(deck=deck, ydkcode=ydkcode, year=year, owner=duelist)
-            deck.save()
+            for deck, ydkcode, year in zip(deck, ydkcode, year):
+                deck = Deck(deck=deck, ydkcode=ydkcode, year=year, owner=duelist)
+                deck.save()
 
-        print(name, nickname, email, cossyId, contact_number)
-        return HttpResponse("Dados enviados com sucesso")
+            print(name, nickname, email, cossyId, contact_number)
+            return HttpResponse("Dados enviados com sucesso")
 
 def update_duelist(request):
     #retorna o duelista na tela (NAO ATAULIZA O VALOR)
