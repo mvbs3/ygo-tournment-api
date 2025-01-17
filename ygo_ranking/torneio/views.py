3af7e77fb8a4a3092364077383cbe30529a997ef
mvbs3 <marcelobs.victor@gmail.com>
2025-01-16 22:34:58 -0300

    atualizando icones e acesso de Staff e usuario comum

--- ygo_ranking/torneio/views.py
+++ ygo_ranking/torneio/views.py
@@ -10,12 +10,13 @@ def novo_torneio(request):
         meu_form = FormTorneio()
         return render(request, 'novo_torneio.html', {'form':meu_form})
     elif request.method == "POST":
-        meu_form = FormTorneio(request.POST)
-        if meu_form.is_valid():
-            meu_form.save()
-            return HttpResponse("Torneio criado com sucesso")
-        else:
-            return render(request, 'novo_torneio.html', {'form':meu_form})
+        if request.user.is_staff:
+            meu_form = FormTorneio(request.POST)
+            if meu_form.is_valid():
+                meu_form.save()
+                return HttpResponse("Torneio criado com sucesso")
+            else:
+                return render(request, 'novo_torneio.html', {'form':meu_form})
 def listar_torneio(request):
     if request.method == "GET":
         torneios = Torneio.objects.all()
